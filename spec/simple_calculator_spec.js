d538a7b8ec843b45345ac06cb0a516b8a5cb9e3a
Anttonertmaphosa <galaletsang.lelo@gmail.com>
2020-04-27 17:09:50 +0200

    final commit

--- spec/simple_calculator_spec.js
+++ spec/simple_calculator_spec.js
@@ -1,19 +1,29 @@
-let {add, multiply} = require("../src/simple_calculator");
+let {add, multiply, addMultiple, multiplyMultiple} = require("../src/simple_calculator");
 
 describe("Add functionality", function() {
-    it("calculates x + y = z", function()  {
-        expect(add(23, 2)).toEqual(26);
+    it("calculates w + x + y = z", function()  {
+        expect(add(23, 2)).toEqual(25);
         expect(add(1, 1)).toEqual(2);
-        expect(add(3, 5)).toEqual(8);
-        expect(add(5, 7)).toEqual(12);
-    })
+        })
 })
 
 describe("Multiply functionality", function()  {
     it("calculates x * y = z", function() {
-        expect(multiply(5, 5)).toEqual(26);
+        expect(multiply(5, 5)).toEqual(25);
         expect(multiply(10, 2)).toEqual(20);
-        expect(multiply(2, 2)).toEqual(4);
-        expect(multiply(6, 2)).toEqual(12);
+    })
+})
+
+describe("add multiple numbers", function()  {
+    it("calculates w + x + y = z", function() {
+        expect(addMultiple(3, 4, 5)).toEqual(12);
+        expect(addMultiple(4, 7, 8)).toEqual(19);
+    })
+})
+
+describe("multiply multiple numbers", function() {
+    it("calculates w * x * y = z", function() {
+        expect(multiplyMultiple(4, 2, 1)).toEqual(8);
+        expect(multiplyMultiple(10, 2, 5)).toEqual(100);
     })
 })
--- src/simple_calculator.js
+++ src/simple_calculator.js
@@ -4,4 +4,11 @@ let add = function(x,y){
 let multiply = function(x,y){
     return (x * y);
 }
-module.exports = {add, multiply}
+let addMultiple = function(w,x,y){
+    return (w + x + y);
+
+}
+let multiplyMultiple = function(w, x, y){
+    return (w * x * y);
+}
+module.exports = {add, multiply, addMultiple, multiplyMultiple}
